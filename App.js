41f52856e5748580303b365592c0e26cdd012370
AjithAj10 <104498680+AjithAj10@users.noreply.github.com>
2022-06-15 16:19:49 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,33 @@
 
+async function create() {
+   
+    let promise = await fetch("./data.json");
+    let data = await promise.json();
+    console.log(data.items[0]);
+
+    for(let i=0; i<data.items.length;i++){
+    let product = "";
+    product = `<div class="product">
+    <div class="img">
+        <img src="${data.items[i].image}" alt="">
+    </div>
+    <div class="content">
+        <h3>${data.items[i].model}</h3>
+        <h5>${data.items[i].price} Rs</h5>
+        <p>${data.items[i].memory}</p>
+        <p>${data.items[i].chipset}</p>
+        <button class="btn btn-primary">Add to cart</button>
+        
+    </div>
+</div>`
+
+productCover.innerHTML += product;
+    }//End of for loop
+
+
+}
+create();
+let page = document.querySelector(".cartPage");
+cartBtn.addEventListener("click", () => {
+    page.classList.toggle('showCart');
+})
